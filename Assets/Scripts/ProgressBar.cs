b46e2661e43d4beea2a0f39585788c419a85b146
rcammack <rcammack@hawaii.edu>
2018-09-12 21:51:45 -1000

    fixed progress bar

--- Assets/Scripts/ProgressBar.cs
+++ Assets/Scripts/ProgressBar.cs
@@ -2,45 +2,40 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class ProgressBar : MonoBehaviour {
+public class ProgressBar : MonoBehaviour
+{
 
-    public int time = 10;
-	public float progress = Manager.Instance.counter / 10;
-    public float speed = 0;
-    bool check = false;
-    public Vector2 position = new Vector2(20, 40);
-    public Vector2 size = new Vector2(200, 20);
-    public Texture2D progress_empty_Image;
-    public Texture2D progress_full_Image;
-	
-    void OnGUI () {
-		var progressBarEmpty = progress_empty_Image;
-		var progressBarFull = progress_full_Image;
-        // draw the background:
-		GUI.BeginGroup(new Rect(position.x, position.y, size.x, size.y));
-		    GUI.Box(new Rect(0, 0, size.x, size.y), progress_empty_Image);
+	public float time = 10; // total seconds to fill up progress bar
+	public float progress; // value between 0 and approximately 1
+	public Vector2 position = new Vector2(20, 50); // position of the progress bar
+	public Vector2 size = new Vector2(10, 10); // total length/width of progress bar
+	public Texture2D progress_empty_Image; // texture for background of progress bar
+	public Texture2D progress_full_Image; // texture for fill of progress bar
 
-		    // draw the filled-in part:
-		    GUI.BeginGroup(new Rect(0, 0, size.x * progress, size.y));
-                GUI.Box(new Rect(0, 0, size.x, size.y), progress_full_Image);
-		    GUI.EndGroup();
-
-		GUI.EndGroup();
-    }
-
-	// Update is called once per frame
-	void Update () {
-        if (check == false) {
-            StartCoroutine(delay());
-        }
+	// initialize the progress based on the percentage of time passed over total time
+	void Start()
+	{
+		if (Manager.Instance.counter != null)
+		{
+			progress = Manager.Instance.counter / time;
+		}
 	}
 
-    IEnumerator delay() {
-        check = true;
-        if (progress < 1) {
-            progress = Manager.Instance.counter / 10;
-        }
-        yield return new WaitForSeconds(speed);
-        check = false;
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		if (progress < 1 && Manager.Instance.counter != null)
+		{
+			progress = Manager.Instance.counter / time;
+		}
+	}
+
+	void OnGUI()
+	{
+		// draw the background
+		GUI.DrawTexture(new Rect(position.x, position.y, size.x, size.y), progress_empty_Image);
+		// fill with appropriate progress
+		GUI.DrawTexture(new Rect(position.x, position.y, size.x * progress, size.y), progress_full_Image);
+	}
+
 }
