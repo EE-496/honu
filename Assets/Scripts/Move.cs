29e122c5c16428884cc357d974d3624523eecce3
Barry Abe <barry.c.a.abe@gmail.com>
2018-10-27 12:09:12 -1000

    Timing game formatted

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class move : MonoBehaviour
-{
+public class Move : MonoBehaviour {
 
     public float delta = 150f;  // Amount to move left and right from the start point
     public float speed = 2.0f;
     Vector2 startPos;
     private bool slide = true;
+    // Use this for initialization
 
     void Start()
     {
@@ -23,17 +23,21 @@ public class move : MonoBehaviour
             v.x += delta * Mathf.Sin(Time.time * speed);
             transform.position = v;
         }
-        if(Input.anyKey){
+        if (Input.anyKey)
+        {
             slide = false;
         }
-        if(!slide){
-            if (transform.position.x < 1.46 && transform.position.x > -2.75) {
+        if (!slide)
+        {
+            if (transform.position.x < 1.52 && transform.position.x > -2.98)
+            {
+                Debug.Log("Win");
                 //move to win screen
             }
-            else{
+            else
+            {
                 //move to lose screen
             }
         }
     }
 }
-
