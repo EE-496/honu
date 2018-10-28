be642fcaf7c1d6ce97b91e66326f6a77e7ca85c7
Sean Teramae <seteramae@gmail.com>
2018-10-27 15:44:31 -1000

    Add fly game

---
+++ Assets/Scripts/SwatTheFly/MoveObject.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour {
+	public GameObject moveObject;
+
+	private Rigidbody2D rb2D;
+	private bool clicked = false;
+
+	// Use this for initialization
+	void Start () {
+		rb2D = gameObject.GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate() {
+		// Vector2 force = new Vector2(Random.Range(-25.0f,25.0f),Random.Range(-25.0f,25.0f));
+        // rb2D.AddForce(force);
+		// Vector3 center3 = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width/2, Screen.height/2, Camera.main.nearClipPlane)); 
+		// Vector2 center2 = new Vector2(center3.x, center3.y);
+		// Vector2 gameObjectPos = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+		// Vector2 direction = new Vector2(center2.x - gameObjectPos.x, center2.y - gameObjectPos.y);
+		// rb2D.AddForce(direction);
+
+		// Keep inside the frame
+		Vector3 pos = Camera.main.WorldToViewportPoint (gameObject.transform.position);
+		pos.x = Mathf.Clamp01(pos.x);
+		pos.y = Mathf.Clamp01(pos.y);
+		gameObject.transform.position = Camera.main.ViewportToWorldPoint(pos);
+    }
+
+	void OnMouseDown() {
+		if(!clicked) {
+			Debug.Log("HERE");
+    		Manager.Instance.successCurrentGame += 1;
+			clicked = true;
+		}
+	}
+}
---
+++ Assets/Scripts/SwatTheFly/SwatTheFly.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwatTheFly : MonoBehaviour {
+	public GameObject fly;
+
+	void Start() {
+		GameObject fly1 = GameObject.Find("Fly1");
+		// fly1.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0, Screen.width), Random.Range(0,Screen.height), Camera.main.nearClipPlane));
+		// fly1.AddComponent<BoxCollider2D>();
+		GameObject fly2 = GameObject.Find("Fly2");
+		GameObject fly3 = GameObject.Find("Fly3");
+
+	}
+
+	void Update() {
+		if(Manager.Instance.successCurrentGame == 3 && Manager.Instance.counter >= Constants.timeForGame){
+			SceneManager.LoadScene("MainMenu");
+		}
+		 else if(Manager.Instance.counter >= Constants.timeForGame) {
+			SceneManager.LoadScene("GameOver");
+		}
+	}	
+}
